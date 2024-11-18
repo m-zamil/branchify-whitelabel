79c91f888e9ae3a4630ca5ffce28ec902d9715b1
Zamil <muzamil-javascriptdev@outlook.com>
2024-11-18 19:22:03 +0500

    image and other optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,32 @@ function updateElement(id, value) {
 const countdownDate = new Date();
 countdownDate.setDate(countdownDate.getDate() + 7); // 7 days from today
 startCountdown(countdownDate);
+/* Video play pause button */
+// Select the button and video elements
+const playBtn = document.getElementById('playBtn');
+const video = document.getElementById('video');
+
+// Add event listener for button click
+playBtn.addEventListener('click', function () {
+  if (video.paused) {
+    // If the video is paused, play the video and hide the button
+    video.play();
+    playBtn.style.display = 'none';
+  } else {
+    // If the video is playing, pause the video and show the button
+    video.pause();
+    playBtn.style.display = 'block';
+  }
+});
+
+video.addEventListener('click', function () {
+  if (video.play) {
+    video.pause();
+    playBtn.style.display = 'block';
+  }
+});
+
+// Optional: Show the button again when the video ends (in case it was hidden)
+video.addEventListener('ended', function () {
+  playBtn.style.display = 'block';
+});
